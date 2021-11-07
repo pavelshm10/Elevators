0526ee2a311383fca46af220ae815de6173a4b92
pavelshm10 <40046519+pavelshm10@users.noreply.github.com>
2021-11-07 20:57:12 +0200

    all working, ui bnugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,84 +13,153 @@ export class AppComponent implements OnInit {
     id:number,
     status: string,
     floor:number,
-    direction:number,
     distance: number,
   }]=[{
     id:1,
     status:'',
     floor:0,
-    direction:0,
     distance: 0,
   }];
   queueOfCalls:any=[]; 
-
+  initPosition: number=0;
+  
   ngOnInit() {
     this.createElevators();
     this.createFloors();
     this.createGrid();
-    // this.setElevatorToCall();
-  }
-
-  setElevatorToCall(){
-    for(let call of this.queueOfCalls) {
-      this.initElevatorsLocation();
-      this.findClosetElevator(call);
-    }
-  }
-
-  initElevatorsLocation(){
-    for(let elevator of this.elevators){
+    const setArrived=()=>{
       var element=document.getElementsByClassName('elevator') as HTMLCollectionOf<HTMLElement>
-      element.item(elevator.id).style.top=element.item(elevator.id).offsetTop-elevator.distance+'px';
+      this.initPosition=+element.item(0).offsetTop;
     }
+    setTimeout(setArrived.bind(undefined), 1000);
   }
 
-  findClosetElevator(call){
-    var nearestDistance=this.rows;
+  findClosetElevator(floorCall){
+    var correctFloorCall=this.rows-floorCall-1;
+    var floorsDistance=this.rows;
     var nearestID=-1;
-    var nearestFloor=-1;
+    var currentDistance=-1
+    var direction='up';
     for(let elevator of this.elevators){
-      if((call.floor-elevator.floor<nearestDistance) && (elevator.floor<=call.floor)){  
-        nearestDistance=call.floor-elevator.floor;
-        nearestFloor=elevator.floor;
+      if((Math.abs((correctFloorCall-elevator.floor))<floorsDistance) && (elevator.status==='stand')){ //(elevator.floor<=correctFloorCall) &&
+        floorsDistance=Math.abs(correctFloorCall-elevator.floor);
         nearestID=elevator.id;
+        currentDistance=elevator.distance;
+        if(elevator.floor>correctFloorCall){
+          direction='down';
+        } else{
+          direction='up';
+        }
       }
     }
-    this.sendElevatorToCall(call.floor,nearestDistance,nearestID);
-    this.queueOfCalls=this.queueOfCalls.filter(({id}) => id !== call.id);
+    if(nearestID==-1){ //if all busy add to que
+        this.queueOfCalls.push({id:this.queueOfCalls.length+1,floor:correctFloorCall});
+    } else{
+      var element=document.getElementsByClassName('elevator') as HTMLCollectionOf<HTMLElement>
+      console.log("offset ", element.item(nearestID).offsetTop);
+      console.log("last distance of elevator ",currentDistance);
+      element.item(nearestID).style.top=element.item(nearestID).offsetTop - currentDistance +'px';
+      this.updateElevatorAndButton(correctFloorCall,floorsDistance,nearestID,currentDistance,direction);
+    }
   }
 
-  sendElevatorToCall(callFloor,nearestDistance,elevatorID){
-    // console.log(this.elevators.find((elevator)=>elevator.id===elevatorID));
-    var distance=nearestDistance*80;
-    document.getElementsByClassName('elevator').item(elevatorID).animate([
-      { transform: 'translateX(0px)' },
-      { transform: 'translateY('+(-distance)+'px)' }
-    ], {
-      duration: 1000,
-      iterations: 1,
-      fill: 'forwards'
-    });
-    this.updateElevatorStatus(elevatorID,callFloor,distance,'busy',1);
+  updateElevatorAndButton(callFloor,floorsDistance,elevatorID,currentDistance,direction){
+    var distanceToMove=floorsDistance*80;
+    this.updateElevatorStatus(elevatorID,callFloor,distanceToMove,'busy');
+    this.updateButtonStatus(callFloor,floorsDistance);
+    this.setTimer(floorsDistance,elevatorID);
+    this.moveElevator(elevatorID,distanceToMove,floorsDistance,direction);
+    
+    const setStand=(callFloor,elevatorID)=>{
+      this.updateElevatorStatus(elevatorID,callFloor,distanceToMove,'stand');
+      if(this.queueOfCalls.length>0){ //check if there are call in que
+        var queCall=this.queueOfCalls.shift();
+        if(queCall.floor>callFloor){
+          this.updateElevatorAndButton(queCall.floor,floorsDistance,elevatorID,currentDistance,'up');
+        } else{
+          this.updateElevatorAndButton(queCall.floor,floorsDistance,elevatorID,currentDistance,'down');
+        }
+      }
+    }
+    setTimeout(setStand.bind(undefined, callFloor,elevatorID),floorsDistance*1000+2000);
+
+    const setArrived=(callFloor,elevatorID)=>{
+      this.updateElevatorStatus(elevatorID,callFloor,distanceToMove,'arrived');
+    }
+    setTimeout(setArrived.bind(undefined, callFloor,elevatorID), floorsDistance*1000);
   } 
 
-  updateElevatorStatus(elevatorID,floorCall,distance,status,direction){
+  moveElevator(elevatorID,distanceToMove,nearestDistance,direction){
+    var elevator = document.getElementsByClassName('elevator') as HTMLCollectionOf<HTMLElement>  
+    var moveMent=0;
+    if(direction==='up'){
+      moveMent=-distanceToMove;
+    } else{
+      moveMent=distanceToMove;
+    }
+    console.log("movement ",moveMent);
+    elevator.item(elevatorID).animate([
+      { transform: 'translateX(0px)' },
+      { transform: 'translateY('+(moveMent)+'px)' }
+    ], {
+      duration: 1000*nearestDistance,
+      iterations: 1,
+      fill: 'forwards',
+    });
+  }
+
+  setTimer(nearestDistance,elevatorID){
+    var minutes = Math.floor((nearestDistance*1000 % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((nearestDistance*1000 % (1000 * 60)) / 1000);
+    var x = setInterval(function() {
+      document.getElementsByClassName("timer").item(elevatorID).innerHTML = minutes + "m " + seconds + "s ";
+      if(minutes>0 && seconds>0){
+        seconds=seconds-1;
+      } else if (minutes>0 && seconds===0){
+        seconds=60;
+        minutes=minutes-1;
+      } else if(minutes===0 && seconds>0){
+        seconds=seconds-1;
+      } else if(minutes===0 && seconds===0){
+        document.getElementsByClassName("timer").item(elevatorID).innerHTML ='';
+        var sound = document.getElementById('beep_audio');
+        // sound.play(); 
+        clearInterval(x);
+      }
+    }, 1000);
+  }
+
+  updateButtonStatus(correctFloorCall,nearestDistance){    
+    //red
+    var button=document.getElementsByClassName('button') as HTMLCollectionOf<HTMLElement>
+    button.item(this.rows-correctFloorCall-1).style.backgroundColor="red";
+    button.item(this.rows-correctFloorCall-1).innerText="await";
+
+
+    //arrived
+    const serArrived=(element,correctFloorCall)=>{
+      element.item(this.rows-correctFloorCall-1).style.backgroundColor="inherit";
+      element.item(this.rows-correctFloorCall-1).style.border="1px solid lightgreen";
+      element.item(this.rows-correctFloorCall-1).style.color="lightgreen";
+      element.item(this.rows-correctFloorCall-1).innerText="arrived";
+    }
+    setTimeout(serArrived.bind(undefined, button,correctFloorCall), nearestDistance*1000);
+
+    //call
+    const setCall=(element,correctFloorCall)=>{
+      element.item(this.rows-correctFloorCall-1).style.backgroundColor="lightgreen";
+      element.item(this.rows-correctFloorCall-1).style.border="2px solid rgb(118, 118, 118)";
+      element.item(this.rows-correctFloorCall-1).style.color="white";
+      element.item(this.rows-correctFloorCall-1).innerText="call";
+    }
+    setTimeout(setCall.bind(undefined, button,correctFloorCall), nearestDistance*1000+2000);   
+  }
+
+  updateElevatorStatus(elevatorID,floorCall,distance,status){
     this.elevators[elevatorID].status=status;
     this.elevators[elevatorID].floor=floorCall;
-    this.elevators[elevatorID].direction=direction;
     this.elevators[elevatorID].distance=distance;
-  }
-
-  insertCall(floorCall){ 
-    var correctFloorCall=this.rows-floorCall-1;
-    if(this.queueOfCalls){
-      this.queueOfCalls.push({id:this.queueOfCalls.length+1,floor:correctFloorCall});
-    } else {
-      this.queueOfCalls=[{id:1,floor:correctFloorCall}];
-    }
-    this.setElevatorToCall();
-  }
-
+  } 
 
   createElevators(){
     for(let i=0;i<this.cols;i++){
@@ -98,7 +167,6 @@ export class AppComponent implements OnInit {
         id: i,
         status:'stand',
         floor:0,
-        direction:0,
         distance: 0,
       }
     }
